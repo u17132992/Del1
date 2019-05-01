82594c6997dddb7934759f36f68f8f82da4158d7
Carli Rousseau <u17106002@tuks.co.za>
2019-05-01 22:17:00 +0200

    Add files via upload

---
+++ PreventionController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using INF354Deliverable1.Models;
+
+namespace INF354Deliverable1.Controllers
+{
+    [RoutePrefix("Api/Prevention")]
+    public class PreventionController : ApiController
+    {
+        Malaria354Entities1 objEntity = new Malaria354Entities1();
+
+
+        [HttpGet]
+        [Route("AllPreventions")]
+        public IQueryable<Prevention> GetPrevention()
+        {
+            try
+            {
+                return objEntity.Preventions;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("GetPreventionsById/{preventionId}")]
+        public IHttpActionResult GetPreventionById(string preventionId)
+        {
+            Prevention objGen = new Prevention();
+            int ID = Convert.ToInt32(preventionId);
+            try
+            {
+                objGen = objEntity.Preventions.Find(ID);
+                if (objGen == null)
+                {
+                    return NotFound();
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Ok(objGen);
+        }
+
+        [HttpPost]
+        [Route("InsertPreventions")]
+        public IHttpActionResult PostPrevention(Prevention data)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                objEntity.Preventions.Add(data);
+                objEntity.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+
+            return Ok(data);
+        }
+
+        [HttpPut]
+        [Route("UpdatePreventions")]
+        public IHttpActionResult PutPreventionMaster(Prevention prevention)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Prevention objGen = new Prevention();
+                objGen = objEntity.Preventions.Find(prevention.PreventionID);
+                if (objGen != null)
+                {
+                    objGen.PreventionDesc = prevention.PreventionDesc;
+
+
+
+                }
+                int i = objEntity.SaveChanges();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok(prevention);
+        }
+        [HttpDelete]
+        [Route("DeletePreventions")]
+        public IHttpActionResult DeletePreventionDelete(int id)
+        {
+            //int PreventionId = Convert.ToInt32(id);  
+            Prevention prevention = objEntity.Preventions.Find(id);
+            if (prevention == null)
+            {
+                return NotFound();
+            }
+
+            objEntity.Preventions.Remove(prevention);
+            objEntity.SaveChanges();
+
+            return Ok(prevention);
+        }
+    }
+}
---
+++ TransmissionController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using INF354Deliverable1.Models;
+
+namespace INF354Deliverable1.Controllers
+{
+    [RoutePrefix("Api/Transmission")]
+    public class TransmissionController : ApiController
+    {
+
+        Malaria354Entities1 objEntity = new Malaria354Entities1();
+
+
+        [HttpGet]
+        [Route("AllTransmissions")]
+        public IQueryable<Transmission> GetTransmission()
+        {
+            try
+            {
+                return objEntity.Transmissions;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("GetTransmissionsById/{transmissionId}")]
+        public IHttpActionResult GetTransmissionById(string transmissionId)
+        {
+            Transmission objGen = new Transmission();
+            int ID = Convert.ToInt32(transmissionId);
+            try
+            {
+                objGen = objEntity.Transmissions.Find(ID);
+                if (objGen == null)
+                {
+                    return NotFound();
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Ok(objGen);
+        }
+
+        [HttpPost]
+        [Route("InsertTransmissions")]
+        public IHttpActionResult PostTransmission(Transmission data)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                objEntity.Transmissions.Add(data);
+                objEntity.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+
+            return Ok(data);
+        }
+
+        [HttpPut]
+        [Route("UpdateTransmissions")]
+        public IHttpActionResult PutTransmissionMaster(Transmission transmission)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Transmission objGen = new Transmission();
+                objGen = objEntity.Transmissions.Find(transmission.TransmissionID);
+                if (objGen != null)
+                {
+                    objGen.TransmissionDesc = transmission.TransmissionDesc;
+
+
+
+                }
+                int i = objEntity.SaveChanges();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok(transmission);
+        }
+        [HttpDelete]
+        [Route("DeleteTransmissions")]
+        public IHttpActionResult DeleteTransmissionDelete(int id)
+        {
+            //int TransmissionId = Convert.ToInt32(id);  
+            Transmission transmission = objEntity.Transmissions.Find(id);
+            if (transmission == null)
+            {
+                return NotFound();
+            }
+
+            objEntity.Transmissions.Remove(transmission);
+            objEntity.SaveChanges();
+
+            return Ok(transmission);
+        }
+    }
+}
